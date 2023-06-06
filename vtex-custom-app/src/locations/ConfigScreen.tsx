1edc9a49fc50881c023e11c4ac61f1b10bbc4fde
Divyansh-Csqt <divyansh.bajpai@codesquaretech.com>
2023-06-06 14:51:18 +0530

    added confiration keys

--- vtex-custom-app/src/locations/ConfigScreen.tsx
+++ vtex-custom-app/src/locations/ConfigScreen.tsx
@@ -4,13 +4,20 @@ import { Heading, Form, Paragraph, Flex, FormControl, TextInput, Grid } from '@c
 import { css } from 'emotion';
 import { /* useCMA, */ useSDK } from '@contentful/react-apps-toolkit';
 
-export interface AppInstallationParameters {}
+export interface AppInstallationParameters {
+  'X-VTEX-API-AppKey': string;
+  'X-VTEX-API-AppToken': string;
+}
 
 const ConfigScreen = () => {
-  const [parameters, setParameters] = useState<AppInstallationParameters>({});
+  const [parameters, setParameters] = useState<AppInstallationParameters>({
+    'X-VTEX-API-AppKey': '',
+    'X-VTEX-API-AppToken': '',
+  });
   const sdk = useSDK<ConfigAppSDK>();
   // const cma = useCMA();
 
+  console.log()
   const onConfigure = useCallback(async () => {
     const currentState = await sdk.app.getCurrentState();
 
@@ -38,19 +45,69 @@ const ConfigScreen = () => {
 
   const limit = 20;
 
+  const saveConfiguration = async () => {
+    const apiUrl = 'https://api.contentful.com/spaces/b7hw33ucy3y5/environments/master/app_installations/2G8AhUS14mq2vtQJgmPw0C';
+    const authToken = 'CFPAT-bOIPI7Vx0IAM5p71TiciS2iSktpRdrTnmiIoGQUWkA4';
+
+    const headers = {
+      Authorization: `Bearer ${authToken}`,
+      'Content-Type': 'application/vnd.contentful.management.v1+json',
+    };
+
+    const body = {
+      parameters: {
+        'X-VTEX-API-AppKey': parameters['X-VTEX-API-AppKey'],
+        'X-VTEX-API-AppToken': parameters['X-VTEX-API-AppToken'],
+      },
+    };
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'PUT',
+        headers,
+        body: JSON.stringify(body),
+      });
+
+      if (response.ok) {
+        // Configuration saved successfully
+      } else {
+        // Handle error case
+      }
+    } catch (error) {
+      // Handle error case
+    }
+  };
+
   return (
-    <Flex flexDirection="column" className={css({})}>
+    <Flex
+      flexDirection="column"
+      alignItems="center" /* Center content vertically */
+      justifyContent="center" /* Center content horizontally */
+      className={css({
+        minHeight: '100vh', /* Set a minimum height to fill the viewport */
+        border: '1px solid #ccc', /* Add a border */
+        padding: '1rem', /* Add padding */
+      })}
+    >
       <div>
         <div style={{ marginLeft: '1rem' }}>
           <Heading>Vtex Connector App Configuration</Heading>
-          <Paragraph>Welcome to your contentful app. This is your config page.</Paragraph>
+          <Paragraph>Welcome to your Contentful app. This is your config page.</Paragraph>
         </div>
         <hr />
-        <h3>Configuration</h3>
+        <h3>{process.env.REACT_APP_ENVIRONMENT_ID}</h3>
         <h5>Storefront Access Token</h5>
         <FormControl>
-          <FormControl.Label isRequired>Storefront Access Token</FormControl.Label>
-          <TextInput defaultValue="Initial value" maxLength={limit} />
+          <FormControl.Label isRequired>X-VTEX-API-AppKey</FormControl.Label>
+          <TextInput
+            value={parameters['X-VTEX-API-AppKey']}
+            onChange={(e) =>
+              setParameters((prevParams) => ({
+                ...prevParams,
+                'X-VTEX-API-AppKey': e.target.value,
+              }))
+            }
+          />
           <Grid columns="auto 80px">
             <FormControl.HelpText>The storefront access token for your Vtex Account</FormControl.HelpText>
             <FormControl.Counter />
@@ -58,32 +115,21 @@ const ConfigScreen = () => {
         </FormControl>
 
         <FormControl>
-          <FormControl.Label isRequired>API Endpoint</FormControl.Label>
-          <TextInput defaultValue="Initial value" maxLength={limit} />
+          <FormControl.Label isRequired>X-VTEX-API-AppToken</FormControl.Label>
+          <TextInput
+            value={parameters['X-VTEX-API-AppToken']}
+            onChange={(e) =>
+              setParameters((prevParams) => ({
+                ...prevParams,
+                'X-VTEX-API-AppToken': e.target.value,
+              }))
+            }
+          />
           <Grid columns="auto 80px">
             <FormControl.HelpText>The Vtex API endpoint</FormControl.HelpText>
             <FormControl.Counter />
           </Grid>
         </FormControl>
-
-        <h3>Assign to fields</h3>
-        <h6>The App can be used with any content type</h6>
-
-        <button
-          className={css({
-            backgroundColor: '#4CAF50',
-            color: 'white',
-            padding: '12px 20px',
-            border: 'none',
-            borderRadius: '4px',
-            cursor: 'pointer',
-          })}
-          onClick={() => {
-            // Implement your button click logic here
-          }}
-        >
-          Save Configuration
-        </button>
       </div>
     </Flex>
   );
